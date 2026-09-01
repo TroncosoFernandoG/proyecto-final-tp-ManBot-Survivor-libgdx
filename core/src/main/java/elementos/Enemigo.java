b1ba7732b853006744b318fecf830f01c0f996cc
SebastianNahuelIuliano <sebastianiuliano1@gmail.com>
2026-08-31 23:01:36 -0300

    Implementación de la clase Enemigo

---
+++ core/src/main/java/elementos/Enemigo.java
@@ -0,0 +1,79 @@
+package elementos;
+
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+
+public class Enemigo {
+
+	    private float posicionX;
+	    private float posicionY;
+
+	    private float velocidad;
+
+	    private float ancho;
+	    private float alto;
+
+	    public Enemigo(float posicionX, float posicionY, float velocidad) {
+
+	        this.posicionX = posicionX;
+	        this.posicionY = posicionY;
+	        this.velocidad = velocidad;
+
+	        this.ancho = 32;
+	        this.alto = 32;
+	    }
+	    
+	    public float obtenerPosicionX() {
+	        return posicionX;
+	    }
+
+	    public float obtenerPosicionY() {
+	        return posicionY;
+	    }
+
+	    public void mover(float movimientoX, float movimientoY) {
+	        posicionX += movimientoX;
+	        posicionY += movimientoY;
+	    }
+	    
+	    public void perseguir(Jugador jugador, float delta) {
+
+	        float diferenciaX = jugador.obtenerPosicionX() - posicionX;
+	        float diferenciaY = jugador.obtenerPosicionY() - posicionY;
+
+	        float movimientoX = 0;
+	        float movimientoY = 0;
+
+	        if (diferenciaX > 0) {
+	            movimientoX = velocidad * delta;
+	        }
+
+	        if (diferenciaX < 0) {
+	            movimientoX = -velocidad * delta;
+	        }
+
+	        if (diferenciaY > 0) {
+	            movimientoY = velocidad * delta;
+	        }
+
+	        if (diferenciaY < 0) {
+	            movimientoY = -velocidad * delta;
+	        }
+
+	        mover(movimientoX, movimientoY);
+	    }
+	    
+	    public void dibujar(ShapeRenderer forma) {
+
+	        forma.begin(ShapeRenderer.ShapeType.Filled);
+
+	        forma.rect(posicionX, posicionY, ancho, alto);
+
+	        forma.end();
+	    }
+	    
+	    public void actualizar(float delta, Jugador jugador) {
+
+	        perseguir(jugador, delta);
+	    }
+}
+
